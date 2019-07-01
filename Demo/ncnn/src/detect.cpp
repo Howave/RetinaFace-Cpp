c4c1408176614591ad8f1c5f1ea897d90fbaa9fe
Hao Zeng <hao.zeng@ulsee.com>
2019-07-01 21:28:35 +0800

    1.add build scripts 2.fix mxnet mnet.25 json file

--- Demo/ncnn/src/detect.cpp
+++ Demo/ncnn/src/detect.cpp
@@ -28,19 +28,19 @@ int main(int argc, char** argv) {
 
     extern float pixel_mean[3];
     extern float pixel_std[3];
-	std::string param_path =  "./models/retina.param";
-	std::string bin_path = "./models/retina.bin";
-	ncnn::Net _net;
-	_net.load_param(param_path.data());
-	_net.load_model(bin_path.data());
+    std::string param_path =  "./models/retina.param";
+    std::string bin_path = "./models/retina.bin";
+    ncnn::Net _net;
+    _net.load_param(param_path.data());
+    _net.load_model(bin_path.data());
 
     ncnn::Mat input = ncnn::Mat::from_pixels(img.data, ncnn::Mat::PIXEL_BGR2RGB, img.cols, img.rows);
-	//ncnn::Mat input = ncnn::Mat::from_pixels_resize(img.data, ncnn::Mat::PIXEL_BGR2RGB, img.cols, img.rows, 300, 300);
+    //ncnn::Mat input = ncnn::Mat::from_pixels_resize(img.data, ncnn::Mat::PIXEL_BGR2RGB, img.cols, img.rows, 300, 300);
     //cv::resize(img, img, cv::Size(300, 300));
 
     input.substract_mean_normalize(pixel_mean, pixel_std);
-	ncnn::Extractor _extractor = _net.create_extractor();
-	_extractor.input(retina_param_id::BLOB_data, input);
+    ncnn::Extractor _extractor = _net.create_extractor();
+    _extractor.input(retina_param_id::BLOB_data, input);
 
 
     std::vector<AnchorGenerator> anchors(_feat_stride_fpn.size());
@@ -54,9 +54,9 @@ int main(int argc, char** argv) {
 
     double start = ncnn::get_current_time();
     for (int i = 0; i < _feat_stride_fpn.size(); ++i) { 
-    	ncnn::Mat cls;
-    	ncnn::Mat reg;
-    	ncnn::Mat pts;
+        ncnn::Mat cls;
+        ncnn::Mat reg;
+        ncnn::Mat pts;
 
         // get blob output
         char clsname[100]; sprintf(clsname, "face_rpn_cls_prob_reshape_stride%d", _feat_stride_fpn[i]);
@@ -96,7 +96,7 @@ int main(int argc, char** argv) {
                            cv::Point2f(result[i].finalbox.width, result[i].finalbox.height),
                            cv::Scalar(0, 255, 255), 2, 8, 0);
         for (int j = 0; j < result[i].pts.size(); ++j) {
-        	cv::circle(img, cv::Point((int)result[i].pts[j].x, (int)result[i].pts[j].y), 1, cv::Scalar(225, 0, 225), 2, 8);
+            cv::circle(img, cv::Point((int)result[i].pts[j].x, (int)result[i].pts[j].y), 1, cv::Scalar(225, 0, 225), 2, 8);
         }
         result[i].print();
     }
